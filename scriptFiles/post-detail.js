f23f88914e6b0376f51c37b019eb1734155142b7
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-02-05 16:59:18 +0100

    stying links and about to add JS to them

--- scriptFiles/post-detail.js
+++ scriptFiles/post-detail.js
@@ -30,6 +30,9 @@ async function fetchPostDetails() {
         document.getElementById('postTitle').textContent = cleanTitle;
         document.getElementById('postContent').innerHTML = post.content.rendered;
 
+       
+        applyStyling();
+
         loader.style.display = 'none'; 
     } catch (error) {
         console.error("Could not fetch the post details: ", error);
@@ -47,4 +50,17 @@ function decodeHtmlEntities(text) {
 
 function removeSpecialCharacters(text) {
     return text.replace(/[^a-zA-Z\s,:;]/g, '');
+}
+
+function applyStyling() {
+
+    const postContent = document.getElementById('postContent');
+    postContent.style.textAlign = 'center';
+
+    const images = postContent.getElementsByTagName('img');
+    for (let img of images) {
+        img.style.display = 'block';
+        img.style.marginLeft = 'auto';
+        img.style.marginRight = 'auto';
+    }
 }
