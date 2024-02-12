c9e2e231e432c67e82092084bf95ad6619013b7c
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-02-12 23:27:34 +0100

    image messing did not work

--- scriptFiles/main.js
+++ scriptFiles/main.js
@@ -8,19 +8,6 @@ import { carouselControls } from './carousel.js';
 async function insertPostTitlesAndImages() {
     const loader = document.getElementById('loader');
     const errorContainer = document.getElementById('error-container') || document.querySelector('#posts-container');
-
-
-    function getImageSizeParams() {
-        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        if (screenWidth <= 480) { 
-            return '?w=200&h=150&format=webp&quality=70'; 
-        } else if (screenWidth <= 768) { 
-            return '?w=300&h=225&format=webp&quality=75';
-        } else {
-            return '?w=400&h=300&format=webp&quality=80';
-        }
-    }
-
     try {
         const response = await fetch(allPostsURL);
         if (!response.ok) {
@@ -28,12 +15,13 @@ async function insertPostTitlesAndImages() {
         }
         const posts = await response.json();
         const postsContainer = document.querySelector('#posts-container');
+
         errorContainer.innerHTML = '';
+
+
 
         let mostRecentPostDate = new Date(Math.max(...posts.map(post => new Date(post.date_gmt))));
         let mostRecentPostId = posts.find(post => new Date(post.date_gmt).getTime() === mostRecentPostDate.getTime()).id;
-
-        const imageSizeParams = getImageSizeParams(); 
 
         posts.forEach(post => {
             const postElement = document.createElement('div');
@@ -41,14 +29,10 @@ async function insertPostTitlesAndImages() {
 
             const imgRegex = /<img.*?src=["'](.*?)["']/;
             const imgMatch = post.content.rendered.match(imgRegex);
-            let imgSrc = imgMatch ? imgMatch[1] : '';
-
-            if (imgSrc) {
-                imgSrc += imageSizeParams; 
-            }
-
+            const imgSrc = imgMatch ? imgMatch[1] : '';
             const imageHTML = imgSrc ? `<a href="sitePages/post-detail.html?postId=${post.id}"><div class="post-image"><img src="${imgSrc}" alt=""></div></a>` : '';
 
+           
             let mostRecentNote = post.id === mostRecentPostId ? " (Our most recent Article)" : "";
             postElement.innerHTML = imageHTML + `<h2>${post.title.rendered}${mostRecentNote}</h2>`;
             postsContainer.appendChild(postElement);
@@ -60,7 +44,8 @@ async function insertPostTitlesAndImages() {
     }
 }
 
-
+async function fetchPostDetails() {
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     const postsContainer = document.querySelector('#posts-container');
